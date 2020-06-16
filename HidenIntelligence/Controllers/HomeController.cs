250143148409fe694db1d890ab0f0a2da34d10d8
weconqueredEurope <58295454+weconqueredEurope@users.noreply.github.com>
2020-06-16 21:54:45 +0700

    Revert "Kết nối website với CSDL"
    This reverts commit 9abb2fe2fa45edfca83137e53f40e22fee311efc.

--- HidenIntelligence/Controllers/HomeController.cs
+++ HidenIntelligence/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
-﻿using HidenIntelligence.Areas.Admin.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,29 +8,7 @@ namespace HidenIntelligence.Controllers
 {
     public class HomeController : Controller
     {
-        Model1 db = new Model1();
         public ActionResult Index()
-        {
-            var model = db.SanPhams.ToList();
-            return View(model);
-        }
-
-        [HttpGet]
-        public ActionResult Index(string search)
-        {
-            var result = db.SanPhams.Where(x => x.TenSP.Contains(search)).ToList();
-            if (result.Count > 0)
-            {
-                return View("SearchResult", result);
-            }
-            else
-            {
-                return View(db.SanPhams.ToList());
-            }
-        }
-
-        //search view
-        public ActionResult SearchResult()
         {
             return View();
         }
@@ -55,20 +32,6 @@ namespace HidenIntelligence.Controllers
         public ActionResult FAQS()
         {
             return View();
-        }
-        
-        public ActionResult Product(int id)
-        {
-            var model = db.SanPhams.Find(id);
-            return View(model);
-        }
-        
-        //Category
-        [ChildActionOnly]
-        public ActionResult Category()
-        {
-            var model = db.TheLoaiSPs.Where(x => x.MaLoai >= 100).ToList();
-            return PartialView(model);
         }
     }
 }
